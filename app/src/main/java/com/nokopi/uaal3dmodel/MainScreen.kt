69b918b6b8a0352c0fb1f978464524b6728abe9b
RyotaHiyama <r_hiyama@yumemi.co.jp>
2024-08-19 13:13:40 +0900

    UnityとAndroidのスクロールを共存

--- app/src/main/java/com/nokopi/uaal3dmodel/MainScreen.kt
+++ app/src/main/java/com/nokopi/uaal3dmodel/MainScreen.kt
@@ -2,11 +2,13 @@ package com.nokopi.uaal3dmodel
 
 import android.view.LayoutInflater
 import android.widget.FrameLayout
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,8 +17,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.unity3d.player.UnityPlayer
 
@@ -30,94 +36,25 @@ fun MainScreen(
         mutableStateOf("")
     }
 
-    Box(
-        modifier = modifier.fillMaxWidth(),
-        contentAlignment = Alignment.BottomCenter,
-    ) {
-        AndroidView(
-            modifier = Modifier
-                .fillMaxSize(),
-            factory = { context ->
-                val view = LayoutInflater.from(context).inflate(R.layout.activity_main, null, false)
-                view.apply {
-                    val unityContainer: FrameLayout = findViewById(R.id.unity)
-                    unityContainer.addView(unityPlayer)
-                }
-            }
-        )
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(
-                onClick = {
-                    UnityPlayer.UnitySendMessage("PlayerController", "ChangePose", "")
-                }
-            ) {
-                Text(text = "Pose Change")
-            }
-            TextField(
-                value = inputText,
-                onValueChange = { setInputText(it) }
-            )
-            Button(
-                onClick = {
-                    UnityPlayer.UnitySendMessage("Canvas", "TestText", inputText)
-                }
-            ) {
-                Text(text = "送信")
-            }
-        }
-    }
-
-    // LazyColumn(
-    //     modifier = modifier
-    //         .fillMaxWidth()
-    //         .nestedScroll(nes),
-    //     verticalArrangement = Arrangement.Center,
-    //     horizontalAlignment = Alignment.CenterHorizontally,
+    // Box(
+    //     modifier = modifier.fillMaxWidth(),
+    //     contentAlignment = Alignment.BottomCenter,
     // ) {
-    //     item {
-    //         val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-    //         val unityViewHeight = screenHeight * 0.8f // 画面の80%の高さ
-    //         AndroidView(
-    //             modifier = Modifier
-    //                 .fillMaxWidth()
-    //                 .height(unityViewHeight)
-    //                 .scrollable(rememberScrollableState {
-    //                     // View component deltas should be reflected in Compose
-    //                     // components that participate in nested scrolling
-    //                     it
-    //                 }, Orientation.Vertical),
-    //             // .pointerInput(Unit) {
-    //             //     // awaitPointerEventScope {
-    //             //     //     val event = awaitPointerEvent(PointerEventPass.Initial)
-    //             //     //     event.changes.forEach {
-    //             //     //         it.consume()
-    //             //     //     }
-    //             //     // }
-    //             //     detectDragGestures { change, _ ->
-    //             //         change.consume()
-    //             //     }
-    //             // },
-    //             // factory = { _ ->
-    //             //     val view = layoutInflater.inflate(R.layout.activity_main, null, false)
-    //             //     val unityContainer: FrameLayout = view.findViewById(R.id.unity)
-    //             //     unityContainer.addView(unityPlayer.view)
-    //             //     view
-    //             // }
-    //             factory = { context ->
-    //                 val view = LayoutInflater.from(context).inflate(R.layout.activity_main, null, false)
-    //                 view.apply {
-    //                     val unityContainer: FrameLayout = findViewById(R.id.unity)
-    //                     unityContainer.addView(unityPlayer.view)
-    //                     // Nested scrolling interop is enabled when nested scroll is enabled for the root View
-    //                     ViewCompat.setNestedScrollingEnabled(this, true)
-    //                 }
+    //     AndroidView(
+    //         modifier = Modifier
+    //             .fillMaxSize(),
+    //         factory = { context ->
+    //             val view = LayoutInflater.from(context).inflate(R.layout.activity_main, null, false)
+    //             view.apply {
+    //                 val unityContainer: FrameLayout = findViewById(R.id.unity)
+    //                 unityContainer.addView(unityPlayer)
     //             }
-    //         )
-    //     }
-    //     item {
+    //         }
+    //     )
+    //     Column(
+    //         verticalArrangement = Arrangement.Center,
+    //         horizontalAlignment = Alignment.CenterHorizontally,
+    //     ) {
     //         Button(
     //             onClick = {
     //                 UnityPlayer.UnitySendMessage("PlayerController", "ChangePose", "")
@@ -125,14 +62,10 @@ fun MainScreen(
     //         ) {
     //             Text(text = "Pose Change")
     //         }
-    //     }
-    //     item {
     //         TextField(
     //             value = inputText,
     //             onValueChange = { setInputText(it) }
     //         )
-    //     }
-    //     item {
     //         Button(
     //             onClick = {
     //                 UnityPlayer.UnitySendMessage("Canvas", "TestText", inputText)
@@ -141,14 +74,69 @@ fun MainScreen(
     //             Text(text = "送信")
     //         }
     //     }
-    //     item {
-    //         Box(
-    //             modifier = Modifier
-    //                 .height(300.dp)
-    //                 .background(Color.Red)
-    //         )
-    //     }
     // }
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        item {
+            val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+            val unityViewHeight = screenHeight * 0.8f // 画面の80%の高さ
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(unityViewHeight)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consume()
+                            UnityPlayer.UnitySendMessage("Canvas", "TestText", "${dragAmount.x},${dragAmount.y}")
+                            UnityPlayer.UnitySendMessage("unitychan", "ObjectRotator", "${dragAmount.y},${-dragAmount.x}")
+                        }
+                    },
+                factory = { context ->
+                    val view = LayoutInflater.from(context).inflate(R.layout.activity_main, null, false)
+                    view.apply {
+                        val unityContainer: FrameLayout = findViewById(R.id.unity)
+                        unityContainer.addView(unityPlayer)
+                    }
+                }
+            )
+        }
+        item {
+            Button(
+                onClick = {
+                    UnityPlayer.UnitySendMessage("unitychan", "ChangePose", "")
+                }
+            ) {
+                Text(text = "Pose Change")
+            }
+        }
+        item {
+            TextField(
+                value = inputText,
+                onValueChange = { setInputText(it) }
+            )
+        }
+        item {
+            Button(
+                onClick = {
+                    UnityPlayer.UnitySendMessage("Canvas", "TestText", inputText)
+                }
+            ) {
+                Text(text = "送信")
+            }
+        }
+        item {
+            Box(
+                modifier = Modifier
+                    .height(300.dp)
+                    .background(Color.Red)
+            )
+        }
+    }
 }
 
 @Preview
